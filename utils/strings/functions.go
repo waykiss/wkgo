ee7c360207ab23713b0003263426d490f57f140e
Rodrigo Rodrigues <rodrigorodriguescosta@gmail.com>
2022-07-27 22:58:38 -0300

    added package utilstrings

--- utils/strings/functions.go
+++ utils/strings/functions.go
@@ -1,6 +1,451 @@
 package utilstrings
 
-import "strings"
+import (
+	"fmt"
+	"github.com/iancoleman/strcase"
+	"github.com/rodrigorodriguescosta/govalidator"
+	"github.com/segmentio/ksuid"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
+	"math"
+	"math/rand"
+	"net/url"
+	"path"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
+	"unicode"
+)
+
+// StringInSlice check if certain string contain in slice
+func StringInSlice(a string, list ...string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
+type RandStringChars string
+
+const (
+	FormatCpf        string = "###.###.###-##"
+	FormatCnpj       string = "##.###.###/####-##"
+	FormatCreditCard string = "####.####.####.####"
+	MaskCreditCard   string = "****.****.****-####"
+)
+
+func (r RandStringChars) String() string {
+	return string(r)
+}
+
+const RandStringCharsOnlyNumbers RandStringChars = "0123456789"
+const RandStringCharsOnlyLetters RandStringChars = "abcdefghijklmnopqrstuvwxyz"
+const RandStringCharsLettersAndNumbers RandStringChars = "abcdefghijklmnopqrstuvwxyz0123456789"
+const RandStringCharsAllLettersAndNumbers RandStringChars = `0123456789ABCDEFGHIJKLMNOPQRSTUVW` +
+	`XYZabcdefghijklmnopqrstuvwxyz`
+
+const (
+	RegexOnlyLetters = "[^A-Z]"
+	RegexOnlyNumbers = "[^0-9]"
+)
+
+//RandString generate a random string
+//Use the constants RandStringCharsOnlyNumbers, RandStringCharsOnlyLetters and RandStringCharsLettersAndNumbers
+//to facilitate to pass the type of character you'd like to generate
+func RandString(n int, chars string) string {
+	result := make([]byte, n)
+	rand.Seed(time.Now().UnixNano())
+	for i := range result {
+		result[i] = chars[rand.Intn(len(chars))]
+	}
+	return string(result)
+}
+
+// RemoveExtraSpaces remove all duplicated spaces over whole string
+func RemoveExtraSpaces(v string) string {
+	return strings.Join(strings.Fields(v), " ")
+}
+
+// NoSpaceNoAccent remove all the spaces of the given string, begin, end and middle of the string
+func NoSpaceNoAccent(v string) string {
+	v = RemoveExtraSpaces(v)
+	v = RemoveAccent(v)
+	return v
+}
+
+//TrimAll remove todos os espacos em uma string
+func TrimAll(v string) string {
+	return strings.ReplaceAll(v, " ", "")
+}
+
+// UpperNoSpaceNoAccent apply 3 steps over string, transform to uppercase, remove duplicate spaces
+// and remove accents
+func UpperNoSpaceNoAccent(v string) string {
+	v = strings.ToUpper(v)
+	v = NoSpaceNoAccent(v)
+	return v
+}
+
+// LowerNoSpaceNoAccent apply 3 steps over string, transform to lowercase, remove duplicate spaces
+// and remove accents
+func LowerNoSpaceNoAccent(v string) string {
+	v = RemoveExtraSpaces(v)
+	v = strings.ToLower(v)
+	v = RemoveAccent(v)
+	return v
+}
+
+// RemoveAccent remove all accents from given string
+func RemoveAccent(v string) string {
+	isMn := func(r rune) bool {
+		return unicode.Is(unicode.Mn, r) // Mn: nonspacing marks
+	}
+
+	t := transform.Chain(norm.NFD, transform.RemoveFunc(isMn), norm.NFC)
+	result, _, _ := transform.String(t, v)
+	return result
+}
+
+/* ReplaceSpecialCharacters substitui todos os caracteres especiais do primeiro parametro pelo valor informado no
+segundo parametro.
+Se o segundo parametro nao for informado, os caracteres serão apenas removidos.
+A partir do terceiro parametro sao consideradas excessoes a regra caso necessario.
+Ex:
+ReplaceSpecialCharacters("_teste!")
+saida: teste
+ReplaceSpecialCharacters("_teste!", "@")
+saida: @teste@
+ReplaceSpecialCharacters("_teste!", "@", "!")
+saida: @teste!
+*/
+func ReplaceSpecialCharacters(v ...string) (result string) {
+	substitute := ""
+	exceptions := ""
+	for i := 0; i < len(v); i++ {
+		switch i {
+		case 0:
+			result = v[i]
+			continue
+		case 1:
+			substitute = v[i]
+			continue
+		default:
+			exceptions += v[i]
+			continue
+		}
+	}
+	result = RemoveAccent(result)
+	re := regexp.MustCompile(fmt.Sprintf(`[^A-Za-z0-9%s]`, exceptions))
+	result = re.ReplaceAllString(result, substitute)
+
+	return
+}
+
+// OnlyNumbers returns only numbers given a string
+func OnlyNumbers(v string) string {
+	if v != "" {
+		reg, _ := regexp.Compile("[^0-9]+")
+		return reg.ReplaceAllString(v, "")
+	}
+	return v
+}
+
+// ContainNumber verifica se uma string contem numeros
+func ContainNumber(v string) bool {
+	if v != "" {
+		reg, _ := regexp.Compile("[0-9]")
+		return reg.MatchString(v)
+	}
+	return false
+}
+
+// PointerToString convert string pointer to string as Zero value if pointer is nil
+func PointerToString(str *string) string {
+	if str == nil {
+		return ""
+	}
+	return *str
+}
+
+func ToString(str interface{}) string {
+	if str == nil {
+		return ""
+	}
+	if value, ok := str.(string); ok {
+		return value
+	}
+	return ""
+}
+
+// StringPToUint convert string pointer to uint, returns zero if pointer is nil
+func StringPToUint(v *string) uint {
+	if v == nil {
+		return 0
+	}
+
+	u64, err := strconv.ParseUint(*v, 10, 32)
+	if err != nil {
+		return 0
+	}
+	return uint(u64)
+}
+
+//GetFilenameFromPath return filename given the path(url or local path)
+func GetFilenameFromPath(p string) string {
+	return path.Base(p)
+}
+
+//GetFilenameWithoutExtension returns the filename without extension of given path
+func GetFilenameWithoutExtension(v string) string {
+	return strings.TrimSuffix(v, path.Ext(v))
+}
+
+//GetPathFromFilename get only the path of the whole path passed by parameter, it returns path without filename
+func GetPathFromFullFilename(v string) string {
+	return strings.TrimSuffix(v, path.Ext(v))
+}
+
+//IsUrl check if the string is a valid url
+func IsUrl(url string) bool {
+	return govalidator.IsURL(url)
+}
+
+//IsInt check if a string is a valid number
+func IsInt(url string) bool {
+	return govalidator.IsInt(url)
+}
+
+func IsOnlyNumbers(v string) bool {
+	if posV := OnlyNumbers(v); posV == v {
+		return true
+	}
+	return false
+}
+
+//ContainNumbers verifica se a string passada contem numeros entre os caracteres
+func ContainNumbers(v string) bool {
+	result := RemoveAccent(v)
+	re := regexp.MustCompile(RegexOnlyNumbers)
+	result = re.ReplaceAllString(result, "")
+	return result != ""
+}
+
+//ContainLetters verifica se a string passada contem letras entre os caracteres
+func ContainLetters(v string) bool {
+	result := RemoveAccent(v)
+	re := regexp.MustCompile(RegexOnlyLetters)
+	result = re.ReplaceAllString(result, "")
+	return result != ""
+}
+
+//ContainNumbersAndLetters verifica se a string passada contem letras e numeros ao mesmo tempo
+func ContainNumbersAndLetters(v string) (r bool) {
+	r = ContainNumbers(v)
+	if !r {
+		return
+	}
+	r = ContainLetters(v)
+	return
+}
+
+var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
+var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
+
+func ToSnakeCase(str string) string {
+	snake := matchFirstCap.ReplaceAllString(str, "${1}_${2}")
+	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
+	return strings.ToLower(snake)
+}
+
+func ToLowerCamelCase(v string) (result string) {
+	var convertNext bool
+	for idx, char := range v {
+		if idx == 0 {
+			result += strings.ToLower(string(char))
+			continue
+		}
+		if char == '_' {
+			convertNext = true
+			continue
+		} else if unicode.IsUpper(char) {
+			if idx+1 < len(v) && unicode.IsLower(rune(v[idx+1])) {
+				result += string(char)
+				continue
+			}
+		}
+
+		if convertNext {
+			convertNext = false
+			result += strings.ToUpper(string(char))
+		} else {
+			result += strings.ToLower(string(char))
+		}
+	}
+	return
+}
+
+func ToCamel(v string) string {
+	return strcase.ToCamel(v)
+}
+
+func Uuid() string {
+	return ksuid.New().String()
+}
+
+//GetLastCharacter retorna os ultimos x/count caracteres de uma string
+func GetLastCharacter(v string, count int) (r string) {
+	if v != "" && count < len(v) {
+		r = v[len(v)-count:]
+	}
+
+	return
+}
+
+func ExtractValue(body string, key string) string {
+	keystr := "\"" + key + "\":[^,;\\]}]*"
+	r, _ := regexp.Compile(keystr)
+	match := r.FindString(body)
+	keyValMatch := strings.Split(match, ":")
+	return strings.ReplaceAll(keyValMatch[1], "\"", "")
+}
+
+//ReplaceAtIndex altera um caractere em uma string baseado no index da mesma
+func ReplaceAtIndex(in string, char rune, idx int) string {
+	out := []rune(in)
+	out[idx] = char
+	return string(out)
+}
+
+//Format essa funcao tem como objetivo mascarar strings em um determinado formato, o caractere # será substituído pelo
+//valor subsequente da string, o caractere * a funcao entende como uma mascara em si, ou seja, ela conta os caracteres
+func Format(v, format string) (result string) {
+	// nao processe caso a quantidade de caracteres for diferente dos valores que devem ser preenchidos na mascara
+	if (strings.Count(format, "#") + strings.Count(format, "*")) != len(v) {
+		result = v
+		return
+	}
+	idx := 0
+	result = format
+	for _, c := range format {
+		// o asterisco, conta o índice pois se trata de mascarar
+		if c == '*' {
+			idx++
+			continue
+		}
+		if c == '#' {
+			charAt := v[idx]
+			result = strings.Replace(result, "#", string(charAt), 1)
+			idx++
+			continue
+		}
+	}
+	return
+}
+
+//
+////GetJsonFromArray funcao que dado um array de string e um campo, retorna um json no formato de string
+//func GetJsonFromArray(fieldName string, array []string) string {
+//	var arrayQuery []map[string]string
+//	for _, value := range array {
+//		arrayQuery = append(arrayQuery, map[string]string{fieldName: value})
+//	}
+//	itemArrayString, _ := json.Marshal(arrayQuery)
+//	return string(itemArrayString)
+//}
+
+//GetNotZeroValue dado um array de valores string, é retornado o que tem valor nao zerovalue
+func GetNotZeroValue(p ...string) string {
+	for _, v := range p {
+		if v != "" {
+			return v
+		}
+	}
+	return ""
+}
+
+// StrPad returns the input string padded on the left, right or both sides using padType to the specified padding length padLength.
+//
+// Example:
+// input := "Codes";
+// StrPad(input, 10, " ", "RIGHT")        // produces "Codes     "
+// StrPad(input, 10, "-=", "LEFT")        // produces "=-=-=Codes"
+// StrPad(input, 10, "_", "BOTH")         // produces "__Codes___"
+// StrPad(input, 6, "___", "RIGHT")       // produces "Codes_"
+// StrPad(input, 3, "*", "RIGHT")         // produces "Codes"
+func StrPad(input string, padLength int, padString string, padType string) string {
+	var output string
+
+	inputLength := len(input)
+	padStringLength := len(padString)
+
+	if inputLength >= padLength {
+		return input
+	}
+
+	repeat := math.Ceil(float64(1) + (float64(padLength-padStringLength))/float64(padStringLength))
+
+	switch padType {
+	case "RIGHT":
+		output = input + strings.Repeat(padString, int(repeat))
+		output = output[:padLength]
+	case "LEFT":
+		output = strings.Repeat(padString, int(repeat)) + input
+		output = output[len(output)-padLength:]
+	case "BOTH":
+		length := (float64(padLength - inputLength)) / float64(2)
+		repeat = math.Ceil(length / float64(padStringLength))
+		output = strings.Repeat(padString, int(repeat))[:int(math.Floor(float64(length)))] + input + strings.Repeat(padString, int(repeat))[:int(math.Ceil(float64(length)))]
+	}
+
+	return output
+}
+
+//StrPadRight adicionar `padLength` quantidade de caracteres a direita de uma string
+func StrPadRight(input string, padLength int, padString string) string {
+	return StrPad(input, padLength, padString, "RIGHT")
+}
+
+// CutString remove parte do texto de acordo com o inicio e fim da sequencia
+//informados, retornando o texto atualizado
+func CutString(str, initial, final string, n int) (strOut string) {
+	strCut := GetStringBetween(str, initial, final, true)
+	//efetua substituição da string capturada por uma string vazia
+	if strCut == "" {
+		return str
+	}
+	strOut = strings.Replace(str, strCut, "", n)
+	return
+}
+
+/*GetStringBetween retorna a string que está entre a string inicial e final passada como parametro, caso nao encontre,
+retorne uma string vazia
+*/
+func GetStringBetween(str, initial, final string, includeInitialAndFinal bool) (strOut string) {
+	iniIndex := strings.Index(str, initial)
+	//se nao encontrar o index da string a ser removida retornar a propria string base
+	if iniIndex < 0 {
+		return
+	}
+	finIndex := iniIndex + strings.Index(str[iniIndex:], final)
+	if finIndex <= iniIndex {
+		return
+	}
+	//captura a sequencia a ser removida
+	strOut = str[iniIndex : finIndex+len(final)]
+	if includeInitialAndFinal == false {
+		strOut = str[iniIndex+(len(initial)) : finIndex]
+	}
+	return
+}
+
+//GetUriFromUrl retorna uma URI dado uma URL
+func GetUriFromUrl(urlString string) string {
+	u, _ := url.Parse(urlString)
+	return u.RequestURI()
+}
 
 //WordCount return the number of the words that contains in string
 func WordCount(v string) int {
--- utils/strings/functions_test.go
+++ utils/strings/functions_test.go
@@ -2,8 +2,192 @@ package utilstrings
 
 import (
 	"github.com/stretchr/testify/assert"
+	"math/rand"
 	"testing"
+	"time"
 )
+
+func TestUpperNoSpaceNoAccent(t *testing.T) {
+	values := []struct {
+		input    string
+		expected string
+	}{
+		{" Hey Holla ÀáÀ  ê ã", "HEY HOLLA AAA E A"},
+		{"Test removing accent Á á ẽ Ê", "TEST REMOVING ACCENT A A E E"},
+		{"Test removing spaces in the middle of the    words", "TEST REMOVING SPACES IN THE MIDDLE OF " +
+			"THE WORDS"},
+		{" Removing spaces in the edges ", "REMOVING SPACES IN THE EDGES"},
+	}
+	for _, v := range values {
+		got := UpperNoSpaceNoAccent(v.input)
+		assert.Equal(t, v.expected, got, "Conversion of the value '%f' is expected '%f' but got '%f'",
+			v.input, v.expected, got)
+	}
+}
+
+func TestReplaceSpecialCharacters(t *testing.T) {
+	values := []struct {
+		input      string
+		substitute string
+		exceptions []string
+		expected   string
+	}{
+		{input: "@teste@", expected: "teste"},
+		{input: "@_teste !", substitute: "_", expected: "__teste__"},
+		{input: "@teste!", substitute: "_", expected: "_teste_"},
+		{input: "_teste!", substitute: "[underline]", expected: "[underline]teste[underline]"},
+		{input: "_teste!", substitute: "[underline]", exceptions: []string{"!"}, expected: "[underline]teste!"},
+		{input: "-áAb-axxb-_@_(_%_&__$ #_`_!a!90${1}", substitute: "_", exceptions: []string{"!"}, expected: "_aAb_axxb_________________!a!90__1_"},
+	}
+	for _, v := range values {
+		values := []string{}
+		values = append(values, v.input)
+		values = append(values, v.substitute)
+		values = append(values, v.exceptions...)
+		result := ReplaceSpecialCharacters(values...)
+		assert.Equal(t, v.expected, result, "Conversion of the value '%s' is expected '%s' but got '%s'",
+			v.input, v.expected, result)
+	}
+}
+
+func TestToSnakeCase(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{"", ""},
+		{"already_snake", "already_snake"},
+		{"A", "a"},
+		{"AA", "aa"},
+		{"AaAa", "aa_aa"},
+		{"HTTPRequest", "http_request"},
+		{"BatteryLifeValue", "battery_life_value"},
+		{"Id0Value", "id0_value"},
+		{"ID0Value", "id0_value"},
+	}
+	for _, test := range tests {
+		have := ToSnakeCase(test.input)
+		if have != test.want {
+			t.Errorf("input=%q:\nhave: %q\nwant: %q", test.input, have, test.want)
+		}
+	}
+}
+
+func TestToLowerCamelCase(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{"", ""},
+		{"already_snake", "alreadySnake"},
+		{"A", "a"},
+		{"AA", "aa"},
+		{"AaAa", "aaAa"},
+		{"AaaAAa", "aaaaAa"},
+		{"HTTPRequest", "httpRequest"},
+		{"BatteryLifeValue", "batteryLifeValue"},
+	}
+	for _, test := range tests {
+		have := ToLowerCamelCase(test.input)
+		if have != test.want {
+			t.Errorf("input=%q:\nhave: %q\nwant: %q", test.input, have, test.want)
+		}
+	}
+}
+
+func TestRandString(t *testing.T) {
+	// testa o length
+	for i := 0; i < 10; i++ {
+		lenth := rand.Int()
+		v := RandString(lenth, RandStringCharsOnlyNumbers.String())
+		assert.Equal(t, lenth, len(v))
+	}
+
+	// testa se os numeros nao sao repetidos sequencialmente
+	var results []string
+	for i := 0; i < 1000; i++ {
+		value := ""
+		for j := 0; j < 4; j++ {
+			v := RandString(2, RandStringCharsOnlyNumbers.String())
+			value += v + "-"
+			//Necessario para que a funcao n tente gerar no mesmo nanosegundo, gerando assim o mesmo numero
+			time.Sleep(time.Nanosecond * 1)
+		}
+		results = append(results, value)
+		for k := 0; k < len(results)-2; k++ {
+			assert.NotEqual(t, results[k], results[i], "Valor gerado nao deveria ser igual ao último gerado")
+		}
+	}
+
+	// testa se as letras nao sao repetidas sequencialmente
+	results = []string{}
+	for i := 0; i < 1000; i++ {
+		value := ""
+		for j := 0; j < 4; j++ {
+			v := RandString(2, RandStringCharsOnlyLetters.String())
+			value += v + "-"
+			//Necessario para que a funcao n tente gerar no mesmo nanosegundo, gerando assim o mesmo numero
+			time.Sleep(time.Nanosecond * 1)
+		}
+		results = append(results, value)
+		for k := 0; k < len(results)-2; k++ {
+			assert.NotEqual(t, results[k], results[i], "Valor gerado nao deveria ser igual ao último gerado")
+		}
+	}
+}
+
+func TestFormatString(t *testing.T) {
+	tests := []struct{ input, mask, expected string }{
+		{"8499999999", "(##) ####-####", "(84) 9999-9999"},
+		{"25740703050", "###.###.###-##", "257.407.030-50"},
+		{"4444555566667777", "####.####.####.####", "4444.5555.6666.7777"},
+		{"4444555566669879", "****.****.****.####", "****.****.****.9879"},
+		{"4444555566669879", "****.####.****.####", "****.5555.****.9879"},
+		{"4444555566669879", "####.****.****.####", "4444.****.****.9879"},
+		{"59605270", "#####-###", "59605-270"},
+		{"06444338000119", "##.###.###/####-##", "06.444.338/0001-19"},
+	}
+	for _, test := range tests {
+		returned := Format(test.input, test.mask)
+		if returned != test.expected {
+			assert.Equalf(t, test.expected, returned, "input=%q:\nreturned: %q\nwant: %q", test.input, returned, test.expected)
+		}
+	}
+}
+
+func TestGetUriFromUrl(t *testing.T) {
+	tests := []struct{ input, expected string }{
+		{"http://localhost:7000/api/linksoft/nfe/danfe?chave=24211019843657000149550010000003981197279070", "/api/linksoft/nfe/danfe?chave=24211019843657000149550010000003981197279070"},
+		{"https://erp.sigelfe.com/api/linksoft/nfe/danfe?chave=123", "/api/linksoft/nfe/danfe?chave=123"},
+		{"http://localhost:7000/teste123", "/teste123"},
+		{"teste123", "teste123"},
+		{"http://localhost:7000", "/"},
+		{"http://localhost:7000/", "/"},
+		{"ff", "ff"},
+	}
+	for _, test := range tests {
+		returned := GetUriFromUrl(test.input)
+		if returned != test.expected {
+			assert.Equalf(t, test.expected, returned, "input=%q:\nreturned: %q\nwant: %q", test.input, returned, test.expected)
+		}
+	}
+
+}
+func TestCutString(t *testing.T) {
+	tests := []struct {
+		str, inicial, final, esperado string
+		n                             int
+	}{
+		{"TESTE (1234) AAAA", "(", ")", "TESTE  AAAA", 1},
+		{"AAAAAAAA BBBBBBB VALOR APROXIMADO DE TRIBUTOS FONTE IBPT", "VALOR APROXIMADO", "FONTE IBPT", "AAAAAAAA BBBBBBB ", 1},
+	}
+	for _, test := range tests {
+		returned := CutString(test.str, test.inicial, test.final, test.n)
+		if returned != test.esperado {
+			assert.Equal(t, test.esperado, returned)
+		}
+	}
+}
 
 func TestWordCount(t *testing.T) {
 	testCases := []struct {
